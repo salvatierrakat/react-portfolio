6886705a82b1f4eb8cc63a15565c85eb07a1853c
salvatierrakat <you@example.com>
2022-07-05 23:02:41 -0500

    component add

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from './portfolio-container';
+
 export default class App extends Component {
   render() {
     return (
